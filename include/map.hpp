213a637cc0c2df3593613c9c1ef76226f8493ce8
DaJourJChristophe <dc198jcn215n@gmail.com>
2026-01-22 07:31:58 -0700

    add submodule deps for hashing and testing

--- include/map.hpp
+++ include/map.hpp
@@ -1,3 +1,33 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
 
+template <typename K, typename V, std::size_t N>
+class Map final
+{
+  enum class BucketState : std::uint8_t
+  {
+    EMPTY,
+    OCCUPIED,
+    TOMBSTONE,
+  };
+
+  struct Bucket final
+  {
+    BucketState state;
+    K           key;
+    V           val;
+  };
+
+  std::array<Bucket, N> m_slots;
+
+public:
+  Map() noexcept = default;
+
+  int get(void) noexcept { return (-1); }
+
+  int set(void) noexcept { return (-1); }
+
+  int del(void) noexcept { return (-1); }
+};
